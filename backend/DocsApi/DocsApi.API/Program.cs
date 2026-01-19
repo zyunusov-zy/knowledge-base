ececa739d7c89111833a27ca6ed1e8042532762f
Zafarbek Yunusov <zyunusov30@gmail.com>
2026-01-19 16:48:26 +0500

    Added docker-compose

--- backend/DocsApi/DocsApi.API/Program.cs
+++ backend/DocsApi/DocsApi.API/Program.cs
@@ -32,7 +32,7 @@ builder.Services.AddCors(options =>
 {
     options.AddPolicy("AllowFrontend", policy =>
     {
-        policy.WithOrigins("http://localhost:5173")
+        policy.WithOrigins("http://localhost:3000")
               .AllowAnyHeader()
               .AllowAnyMethod()
               .AllowCredentials();
